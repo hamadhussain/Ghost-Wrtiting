f9428463e317679c2aa81f676a6e720540b7ae79
hamad <hammad31012004@gmail.com>
2025-09-24 12:51:46 +0500

    update

---
+++ src/app/Components/Portfolio/Embla/page.jsx
@@ -0,0 +1,107 @@
+'use client'
+import React, { useCallback, useEffect, useState } from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
+
+
+
+const OPTIONS = { dragFree: true, loop: true }
+
+const useDotButton = (emblaApi, onButtonClick) => {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [scrollSnaps, setScrollSnaps] = useState([])
+
+  const onDotButtonClick = useCallback(
+    (index) => {
+      if (!emblaApi) return
+      emblaApi.scrollTo(index)
+      if (onButtonClick) onButtonClick(emblaApi)
+    },
+    [emblaApi, onButtonClick]
+  )
+
+  const onInit = useCallback((emblaApi) => {
+    setScrollSnaps(emblaApi.scrollSnapList())
+  }, [])
+
+  const onSelect = useCallback((emblaApi) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap())
+  }, [])
+
+  useEffect(() => {
+    if (!emblaApi) return
+
+    onInit(emblaApi)
+    onSelect(emblaApi)
+    emblaApi.on('reInit', onInit).on('reInit', onSelect).on('select', onSelect)
+  }, [emblaApi, onInit, onSelect])
+
+  return {
+    selectedIndex,
+    scrollSnaps,
+    onDotButtonClick
+  }
+}
+
+const DotButton = ({ children, ...rest }) => (
+  <button type="button" {...rest}>
+    {children}
+  </button>
+)
+
+export default function Home({slide}) {
+  const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [Autoplay()])
+   console.log("slide",slide);
+   const SLIDES=slide;
+  const onNavButtonClick = useCallback((emblaApi) => {
+    const autoplay = emblaApi?.plugins()?.autoplay
+    if (!autoplay) return
+
+    const resetOrStop =
+      autoplay.options.stopOnInteraction === false
+        ? autoplay.reset
+        : autoplay.stop
+
+    resetOrStop()
+  }, [])
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
+    emblaApi,
+    onNavButtonClick
+  )
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {SLIDES.map((src, index) => (
+            <div className="embla__slide" key={index}>
+              <div className="embla__slide__image-wrapper">
+                <img
+                  className="embla__slide__img"
+                  src={src}
+                  alt={`Book Cover ${index + 1}`}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={
+                'embla__dot' +
+                (index === selectedIndex ? ' embla__dot--selected' : '')
+              }
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
